a4da646d7f590be8fec2790b1403e0e272c1f0b1
zaksg <zaksg@post.bgu.ac.il>
2018-06-06 13:29:44 +0300

    chceck upload

--- main.py
+++ main.py
@@ -109,6 +109,7 @@ class Clustering:
 
             self.complete_ready_data['Clustering'] = model.labels_
             print self.complete_ready_data
+            print "guy nadav"
 
 root = Tk()
 root.geometry("900x580")
